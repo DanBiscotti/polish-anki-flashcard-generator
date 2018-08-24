a4eaa12076c0eaded7b40d3fa290f5095e77f5d4
Dan Scott <danbiscotti@gmail.com>
2018-08-24 09:30:43 +0100

    Added class for verbs which contains options for plurals, gender and tense

--- polish-sentences.py
+++ polish-sentences.py
@@ -1,14 +1,5 @@
-class Noun:
-    def __init__(self,english,nominative,genetive,dative,accusative,instrumental,locative,vocative,plural):
-        self.english=english
-        self.nominative=nominative
-        self.accusative=accusative
-        self.dative=dative
-        self.genetive=genetive
-        self.instrumental=instrumental
-        self.locative=locative
-        self.vocative=vocative
-        self.plural=plural
+from enum import IntEnum
+import random
 
 on = "ą"
 en = "ę"
@@ -20,16 +11,66 @@ sh = "ś"
 zi = "ź"
 zo = "ż"
 
+class Noun:
+    
+    # Constructor
+    def __init__(self,english,nominative,genetive,dative,accusative,instrumental,locative,vocative,plural,gender):
+        self.english=english
+        self.nominative=nominative
+        self.accusative=accusative
+        self.dative=dative
+        self.genetive=genetive
+        self.instrumental=instrumental
+        self.locative=locative
+        self.vocative=vocative
+        self.plural=plural
+        self.gender=gender
+
+    def copula(self):
+        if(self.plural):
+            return "s"+on+" "
+        else:
+            return "jest "
+
+
+class Tense(IntEnum):
+    PAST=0
+    PRESENT=1
+    FUTURE=2
+
+class Gender(IntEnum):
+    MALE=0
+    FEMALE=1
+    NEUTER=2
+
+class Verb:
+
+    # Constructor
+    def __init__(self,english,variants):
+        self.english=english
+        self.variants=variants
+
+    def getVerb(self,tense,gender,plural):
+        return self.variants[tense][gender][plural]
+
+
 nouns = list()
-nouns.append(Noun("kitten","kotek","kotka","kotkowi","kotka","kotkiem","kotku","kotku",False))
-nouns.append(Noun("book","ksi"+on+zo+"ka","ksi"+on+zo+"ki","ksi"+on+zo+"ce","ksi"+on+zo+"k"+en,"ksi"+on+zo+"k"+on,"ksi"+on+zo+"ce","ksi"+on+zo+"ko",False))
-nouns.append(Noun("egg","jajko","jajka","jajku","jajko","jajkiem","jajku","jajko",False))
-nouns.append(Noun("penises","penisy","penis"+oo+"w","penisom","penisy","penisami","penisach","penisy",True))
-nouns.append(Noun("pussies","cipki","cipek","cipkom","cipki","cipkami","cipkach","cipki",True))
+nouns.append(Noun("kitten","kotek","kotka","kotkowi","kotka","kotkiem","kotku","kotku",False,Gender.MALE))
+nouns.append(Noun("book","ksi"+on+zo+"ka","ksi"+on+zo+"ki","ksi"+on+zo+"ce","ksi"+on+zo+"k"+en,"ksi"+on+zo+"k"+on,"ksi"+on+zo+"ce","ksi"+on+zo+"ko",False,Gender.FEMALE))
+nouns.append(Noun("egg","jajko","jajka","jajku","jajko","jajkiem","jajku","jajko",False,Gender.NEUTER))
+nouns.append(Noun("penises","penisy","penis"+oo+"w","penisom","penisy","penisami","penisach","penisy",True,Gender.MALE))
+nouns.append(Noun("pussies","cipki","cipek","cipkom","cipki","cipkami","cipkach","cipki",True,Gender.FEMALE))
+
+verbs = list()
+#verbs.appends(Verb("To be","jest","by"+wu,"b"+en+"dzie"))
+
+verb = Verb("",[[["by"+wu,"by"+wu+"y"],["by"+wu+"a","by"+wu+"y"],["by"+wu+"o","by"+wu+"y"]],[["jest","s"+on],["jest","s"+on],["jest","s"+on]],[["b"+en+"dzie","b"+en+"d"+on],["b"+en+"dzie","b"+en+"d"+on],["b"+en+"dzie","b"+en+"d"+on]]])
+
+
 
 for noun in nouns:
     print(noun.english.upper()+":")
-    print("To "+("jest " if not noun.plural else "s"+on+" ")+noun.nominative)
+    print("To "+noun.copula()+noun.nominative)
     print("Nie ma "+noun.genetive)
     print("Przygl"+on+"dam si"+en+" "+noun.dative)
     print("Widz"+en+" "+noun.accusative)
@@ -37,3 +78,8 @@ for noun in nouns:
     print("My"+sh+"l"+en+" o "+noun.locative)
     print("O "+noun.vocative+"!")
     print()
+
+for noun in nouns:
+    print("To "+verb.getVerb(int(Tense.PAST),int(noun.gender),int(noun.plural))+" "+noun.nominative)
+    print("To "+verb.getVerb(int(Tense.PRESENT),int(noun.gender),int(noun.plural))+" "+noun.nominative)
+    print("To "+verb.getVerb(int(Tense.FUTURE),int(noun.gender),int(noun.plural))+" "+noun.nominative)
